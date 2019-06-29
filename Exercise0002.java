8210b3b011a6781fddbe9467286686eb92341414
Gustav Björklind <cga1910@users.noreply.github.com>
2019-06-29 23:16:17 +0200

    Add files via upload

---
+++ Exercise0002.java
@@ -0,0 +1,39 @@
+// 2019-06-29
+
+// Write a method that returns the tree root value, i.e. (4 + 2) * (3 + 7) = 60
+
+//    4   2  3   7   <-- leaves
+//    \  /   \  /
+//     +      +
+//      \    /
+//        *
+
+public class Exercise0002 {
+
+  public static void main(String[] args) {
+    int[][] leaves = { {4, 2},
+                       {3, 7} };
+
+    System.out.println(solution(leaves));
+  }
+
+  static int solution(int[][] leaves) {
+    int[] sumArray = new int[leaves.length];
+    int product = 1;
+
+    for (int i = 0; i < leaves.length; i++) {
+      int sum = 0;
+      for (int j = 0; j < leaves[i].length; j++) {
+        sum = sum + leaves[i][j];
+      }
+      sumArray[i] = sum;
+    }
+
+    for (int i = 0; i < sumArray.length; i++) {
+      product = product * sumArray[i];
+    }
+
+    return product;
+  }
+
+}
